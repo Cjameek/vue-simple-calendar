bdc77ca8f7b410b3c61cb051c6c7e8dc591afd4f
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-08-22 11:16:46 -0400

    Add remaining logic

--- src/components/CalendarMonth.vue
+++ src/components/CalendarMonth.vue
@@ -14,7 +14,12 @@
     <CalendarWeekdays/>
 
     <ol class="calendar__days-list calendar-days">
-      <!-- <CalendarMonthDayItem/> -->
+      <CalendarMonthDayItem
+        v-for="day in days"
+        :key="day.date"
+        :day="day"
+        :is-today="day.date === today"
+      />
     </ol>
   </section>
 </template>
@@ -23,10 +28,10 @@
 import dayjs from "dayjs";
 import weekday from "dayjs/plugin/weekday";
 import weekOfYear from "dayjs/plugin/weekOfYear";
-
 import CalendarDateSelected from './CalendarDateSelected';
 import CalendarDatePagination from './CalendarDatePagination';
 import CalendarWeekdays from './CalendarWeekdays';
+import CalendarMonthDayItem from './CalendarMonthDayItem';
 
 dayjs.extend(weekday);
 dayjs.extend(weekOfYear);
@@ -36,7 +41,8 @@ export default {
   components: {
     CalendarDateSelected,
     CalendarDatePagination,
-    CalendarWeekdays
+    CalendarWeekdays,
+    CalendarMonthDayItem
   },
   data() {
     return {
@@ -47,6 +53,62 @@ export default {
   methods: {
     selectDate(newDateSelection) {
       this.selectedDate = newDateSelection;
+    },
+    getWeekday(date) {
+      return dayjs(date).weekday();
+    }
+  },
+  computed: {
+    days() {
+      return [
+        ...this.previousMonthDays,
+        ...this.currentMonthDays,
+        ...this.nextMonthDays
+      ]
+    },
+    month() {
+      return Number(this.selectedDate.format("M"));
+    },
+    year() {
+      return Number(this.selectedDate.format("YYYY"));
+    },
+    numberOfDaysInMonth() {
+      return dayjs(this.selectedDate).daysInMonth();
+    },
+    previousMonthDays() {
+      const firstDayOfTheMonthWeekday = this.getWeekday(this.currentMonthDays[0].date);
+      const previousMonth = dayjs(`${this.year}-${this.month}-01`).subtract(1, "month");
+      const previousMonthLastMondayDayOfMonth = dayjs(this.currentMonthDays[0].date).subtract(firstDayOfTheMonthWeekday - 1, "day").date();
+
+      // Cover first day of the month being sunday 
+      (firstDayOfTheMonthWeekday === 0)
+      const visibleNumberOfDaysFromPreviousMonth = firstDayOfTheMonthWeekday ? firstDayOfTheMonthWeekday - 1 : 6;
+      return [...Array(visibleNumberOfDaysFromPreviousMonth).keys()].map((day) => {
+        return {
+          date: dayjs(`${previousMonth.year()}-${previousMonth.month() + 1}-${previousMonthLastMondayDayOfMonth + day}`).format("YYYY-MM-DD"),
+          isCurrentMonth: false
+        };
+      });
+    },
+    currentMonthDays() {
+      return [...Array(this.numberOfDaysInMonth).keys()].map((day) => {
+        return {
+          date: dayjs(`${this.year}-${this.month}-${day}`).format("YYYY-MM-DD"),
+          isCurrentMonth: true
+        }
+      });
+    },
+    nextMonthDays() {
+      const lastDayOfTheMonthWeekday = this.getWeekday(`${this.year}-${this.month}-${this.currentMonthDays.length}`);
+      const nextMonth = dayjs(`${this.year}-${this.month}-01`).add(1, "month");
+      const visibleNumberOfDaysFromNextMonth = lastDayOfTheMonthWeekday ? 7 - lastDayOfTheMonthWeekday : lastDayOfTheMonthWeekday;
+
+      return [...Array(visibleNumberOfDaysFromNextMonth).keys()].map((day) => {
+        return {
+          date: dayjs(`${nextMonth.year()}-${nextMonth.month() + 1}-${day + 1}`).format("YYYY-MM-DD"),
+          isCurrentMonth: false
+        };
+      });
     }
   }
 }
