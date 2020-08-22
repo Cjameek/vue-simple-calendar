d4acf413f24e1e9f296f6b5080214ccc2aa9ccd7
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-08-22 10:45:27 -0400

    Add CalendarMonthDayItem

---
+++ src/components/CalendarMonthDayItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <li class="calendar-days__day" :class="{'calendar-day__day--not-current': !isCurrentMonth, 'calendar-day__day--today': isCurrentDay}">
+      <span class="calendar-days__label">{{label}}</span>
+  </li>
+</template>
+
+<script>
+import dayjs from "dayjs";
+export default {
+    name: 'CalendarMonthDayItem',
+    props: {
+        day: {
+            type: Object,
+            required: true
+        },
+        isCurrentMonth: {
+            type: Boolean,
+            required: true
+        },
+        isCurrentDay: {
+            type: Boolean,
+            required: true
+        }
+    },
+    computed: {
+        label() {
+            return dayjs(this.day.date).format("D");
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
