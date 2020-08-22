5ce054f2038c0cd147aeb0b8827d4abd3730b483
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-08-22 10:02:52 -0400

    Add DateSelected component

---
+++ src/components/CalendarDateSelected.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="calendar-current-date">{{ selectedMonth }}</div>
+</template>
+
+<script>
+export default {
+    props: {
+        selectedDate: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        selectedMonth() {
+            return this.selectedDate.format("MMMM YYYY");
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
