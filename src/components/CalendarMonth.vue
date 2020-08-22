4114cf1459ae31e4253efcc10a21284fe3240df1
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-08-22 10:03:03 -0400

    Add CalendarMonth component

---
+++ src/components/CalendarMonth.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="calendar-month">
+    <div class="calendar-header">
+      <CalendarDateSelected />
+      <!-- <CalendarDatePagination /> -->
+    </div>
+
+    <!-- <CalendarWeekdays/> -->
+
+    <ol>
+      <!-- <CalendarGrid/> -->
+    </ol>
+  </section>
+</template>
+
+<script>
+import CalendarDateSelected from './CalendarDateSelected.vue'
+
+export default {
+  props: {
+    //msg: String
+  },
+  components: {
+    CalendarDateSelected
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
