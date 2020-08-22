40043e49197bb6346160019eaee88323c59e9d34
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-08-22 18:39:44 -0400

    Add CalendarLegend

---
+++ src/components/CalendarLegend.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="calendar__legend calendar-legend">
+      <div class="calendar-legend__item">
+          <span class="calendar-legend__text">Current Day</span>
+          <span class="calendar-legend__icon" aria-hidden="true"></span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "CalendarLegend",
+    props: {
+        legends: Object
+    }
+}
+</script>
