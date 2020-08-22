55ec9f6df9b685455dd153c3cc889d7dbe60a65a
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-08-22 17:23:07 -0400

    Add CalendarMonthRow

---
+++ src/components/CalendarMonthRow.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="calendar__row">
+    <CalendarMonthDayItem
+        v-for="day in getAllDays"
+        :key="day.date"
+        :day="day"
+        :is-current-month="day.isCurrentMonth"
+        :is-current-day="day.date === getCurrentDay"
+      />
+  </div>
+</template>
+
+<script>
+import CalendarMonthDayItem from './CalendarMonthDayItem';
+export default {
+    name: 'CalendarMonthRow',
+    components: {
+        CalendarMonthDayItem
+    },
+    props: {
+        row: {
+            type: Array,
+            required: true
+        },
+        today: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+        getAllDays() {
+            return this.row;
+        },
+        getCurrentDay() {
+            return this.today;
+        }
+    }
+}
+</script>
